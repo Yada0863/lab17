a39e481a07685a12dc01276a317d8ac6c4593e35
Yada <yada_pholam@cmu.ac.th>
2024-02-06 16:58:10 +0700

    finished

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,20 @@
+vector<int>  randomVector(unsigned int N){	
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
 
-	C = new int;
+void showVector(vector<int>x){
+	cout << "[";
+	for(unsigned int i = 0; i < x.size() ; i++){
+		cout << x[i];
+		if(i == x.size()) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> x,vector<int> y){
+	int sum = 0;
+	for(unsigned int i = 0; x.size(); i++) sum += x[i]*y[i];	
+	return sum;
+}
